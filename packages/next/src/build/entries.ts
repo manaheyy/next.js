7bc0b66727c452e3d1b43a02fd3704f08dc26dfd
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-10-20 17:15:54 -0700

    fixup

--- packages/next/src/build/entries.ts
+++ packages/next/src/build/entries.ts
@@ -45,6 +45,7 @@ import {
   isMiddlewareFilename,
   isInstrumentationHookFile,
   isInstrumentationHookFilename,
+  isProxyFilename,
 } from './utils'
 import { getPageStaticInfo } from './analysis/get-page-static-info'
 import { normalizePathSep } from '../shared/lib/page-path/normalize-path-sep'
@@ -763,6 +764,11 @@ export function runDependingOnPageType<T>(params: {
   ) {
     params.onServer()
     params.onEdgeServer()
+    return
+  }
+
+  if (isProxyFilename(params.page)) {
+    params.onServer()
     return
   }
 
